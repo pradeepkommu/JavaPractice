7a080d980387c641e4a5a75f864dbc887dffc7ac
S00344 <S00344@MC01LAP0411.Mercator.com>
2018-06-14 11:20:49 +0400

    New Files

---
+++ New_Java/src/ReverseNumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ReverseNumber {
+	public static void main(String[] args) {
+		System.out.println("Please enter the number to be reversed");
+		Scanner scan = new Scanner(System.in);
+		int newNumber= scan.nextInt();
+		int rev=0;
+		while(newNumber!=0) {
+			rev=rev*10+newNumber%10;
+			newNumber=newNumber/10;
+		}
+		System.out.println("Reverse of a number is :"+rev);
+		scan.close();
+	}
+}
---
+++ New_Java/src/ReverseString.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ReverseString {
+	public static void main(String[] args) {
+		System.out.println("Enter the String to be reversed..");
+		Scanner scan=new Scanner(System.in);
+		String stringEntered=scan.next();
+		String revString="";
+		for(int i=stringEntered.length()-1;i>=0;i--) {
+			revString=revString+stringEntered.charAt(i);
+		}
+		System.out.println("Reverse String :"+revString);
+		scan.close();
+	}
+}
