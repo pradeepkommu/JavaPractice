e3f92b56eeaf27f5682616b9dd2d658d6b1ad030
S00344 <S00344@MC01LAP0411.Mercator.com>
2018-06-14 10:56:30 +0400

    new change

--- New_Java/src/Palindrome.java
+++ New_Java/src/Palindrome.java
@@ -4,7 +4,7 @@ public class Palindrome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Please enter the string");
+		System.out.println("Please enter the string need to be tested");
 		Scanner scan = new Scanner(System.in);
 		String newString= scan.nextLine();
 		String reverseString="";
